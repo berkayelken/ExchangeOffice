3dd136033345e9698c43c947b9786a7afff51fee
DELL <DELL@192.168.1.254>
2021-10-30 23:14:23 +0300

    Create And Configure Spring Boot Application

---
+++ CurrencyService/src/main/java/com/byelken/exchange/office/CurrencyServiceApplication.java
@@ -0,0 +1,15 @@
+package com.byelken.exchange.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyServiceApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CurrencyServiceApplication.class, args);
+	}
+
+}
